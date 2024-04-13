b9e11b03b88de11f2e5a7a6293e0162badb648de
csouza <ccs1201@gmail.com>
2024-04-13 16:00:45 -0300

    #2 - Adicionado call back para a entidade contrato no pre persist

--- src/main/java/br/com/ccs/contratocobrancaparcela/auditoria/Auditoria.java
+++ src/main/java/br/com/ccs/contratocobrancaparcela/auditoria/Auditoria.java
@@ -1,11 +1,13 @@
 package br.com.ccs.contratocobrancaparcela.auditoria;
 
-import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
 
+import java.time.LocalDateTime;
+@Slf4j
 public class Auditoria {
 
     public static void registrarAuditoria(String operacao, LocalDateTime dataHora, String id, Object object) {
         // Implemente aqui a lógica para registrar a auditoria
-        System.out.println("Operação: " + operacao + ", Data/Hora: " + dataHora + ", Class Type: " + object.getClass().getSimpleName() + ", id: " + id);
+        log.info("Operação: " + operacao + ", Data/Hora: " + dataHora + ", Class Type: " + object.getClass().getSimpleName() + ", id: " + id);
     }
 }
